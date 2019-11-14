1411e9173167796d2135d24d8a9b670d7b0f9187
Leandro Klyte <klyte45@hotmail.com>
2019-11-13 22:49:24 -0300

    v1.1 Texture atlas editing support

--- KlyteFramework.cs
+++ KlyteFramework.cs
@@ -1,18 +1,26 @@
+using ColossalFramework.UI;
 using Klyte.Commons.Extensors;
 using Klyte.Commons.i18n;
 using Klyte.Commons.Interfaces;
 using Klyte.Commons.Utils;
 using Klyte.Framework.Utils;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using UnityEngine;
+using static ColossalFramework.UI.UITextureAtlas;
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
 namespace Klyte.Framework
 {
     public class KlyteFramework : BasicIUserModSimplified<KlyteFramework, KFResourceLoader, MonoBehaviour>
     {
         public KlyteFramework() => Construct();
+
+        internal static readonly string m_localImageFilesPath = $"{FileUtils.BASE_FOLDER_PATH}kf_imageFiles{Path.DirectorySeparatorChar}";
+        internal static readonly string m_atlasExportPath = $"{FileUtils.BASE_FOLDER_PATH}kf_exportedAtlas{Path.DirectorySeparatorChar}";
 
         private bool m_alreadyLoadedTranslations = false;
 
@@ -30,20 +38,29 @@ namespace Klyte.Framework
             {
                 ScanTranslations();
             }
+            ScanImagesForTextureAtlas();
+
+            ext.AddButton("Go to GitHub page!", () => Application.OpenURL("https://github.com/klyte45/KlyteFramework"));
             ext.AddButton("Reload translation files", ScanTranslations);
+            ext.AddButton("Reload images files", ScanImagesForTextureAtlas);
+            ext.AddButton("Export game images", ExportGameImages);
         }
 
         private void ScanTranslations()
         {
             FileUtils.ScanPrefabsFoldersDirectoryNoLoad("kf_translationFiles", (path, pack, asset) =>
             {
-                foreach (var lang in KlyteLocaleManager.locales)
+                foreach (string lang in KlyteLocaleManager.locales)
                 {
-                    var fileName = Path.Combine(path, $"{lang}.txt");
+                    foreach (string file in Directory.GetFiles($"{KlyteLocaleManager.m_translateFilesPath}{lang}{Path.DirectorySeparatorChar}", $"9_KF_*.txt"))
+                    {
+                        File.Delete(file);
+                    }
+                    string fileName = Path.Combine(path, $"{lang}.txt");
                     LogUtils.DoLog("PATH: " + fileName);
                     if (File.Exists(fileName))
                     {
-                        var content = File.ReadAllText(fileName);
+                        string content = File.ReadAllText(fileName);
                         if (content != null)
                         {
                             File.WriteAllText($"{KlyteLocaleManager.m_translateFilesPath}{lang}{Path.DirectorySeparatorChar}9_KF_{(asset.isWorkshopAsset ? $"{(~pack.GetPublishedFileID().AsUInt64).ToString("00000000000000000")}_{asset.name}" : asset.name)}.txt", content);
@@ -54,6 +71,59 @@ namespace Klyte.Framework
             });
             KlyteLocaleManager.ReloadLanguage(true);
             m_alreadyLoadedTranslations = true;
+        }
+
+        private void ScanImagesForTextureAtlas()
+        {
+            var newSprites = new List<SpriteInfo>();
+            FileUtils.ScanPrefabsFoldersDirectoryNoLoad("kf_imageFiles", (path, pack, asset) =>
+            {
+                try
+                {
+                    TextureAtlasUtils.LoadPathTexturesIntoInGameTextureAtlas($"PACK.{(asset.isWorkshopAsset ? $"{(pack.GetPublishedFileID().AsUInt64).ToString("00000000000000000")}" : asset.name)}", path, ref newSprites);
+                }
+                catch (Exception e)
+                {
+                    LogUtils.DoErrorLog($"An error occurred trying to load path \"{path}\": {e.GetType()} => {e.Message}");
+                }
+            });
+            TextureAtlasUtils.LoadPathTexturesIntoInGameTextureAtlas($"KF_LOCAL", m_localImageFilesPath, ref newSprites);
+            if (newSprites.Count > 0)
+            {
+                TextureAtlasUtils.RegenerateDefaultTextureAtlas(newSprites);
+            }
+        }
+
+        private void ExportGameImages()
+        {
+            var borders = new Dictionary<string, RectOffset>();
+            UITextureAtlas defaultTextureAtlas = UIView.GetAView().defaultAtlas;
+            if (defaultTextureAtlas == null)
+            {
+                return;
+            }
+            if (Directory.Exists(m_atlasExportPath))
+            {
+                Directory.Delete(m_atlasExportPath, true);
+            }
+            Directory.CreateDirectory(m_atlasExportPath);
+            foreach (SpriteInfo spriteInfo in defaultTextureAtlas.sprites)
+            {
+                if (spriteInfo.texture == null)
+                {
+                    continue;
+                }
+                File.WriteAllBytes($"{m_atlasExportPath}%{spriteInfo.name}.png", spriteInfo.texture.EncodeToPNG());
+                if (spriteInfo.border != null && (spriteInfo.border.left > 0 || spriteInfo.border.top > 0 || spriteInfo.border.right > 0 || spriteInfo.border.bottom > 0))
+                {
+                    borders[spriteInfo.name] = spriteInfo.border;
+                }
+            }
+            if (borders.Count > 0)
+            {
+                File.WriteAllLines($"{m_atlasExportPath}{TextureAtlasUtils.BORDER_FILENAME}", borders.Select(x => $"{x.Key}={x.Value.left},{x.Value.right},{x.Value.top},{x.Value.bottom}").ToArray());
+            }
+
         }
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Klyte's Framework
+﻿# Klyte's Framework
 This mod is a basic tool for asset makers that want to use simple features from Klyte Commons Includable framework (5th gen mods).
 Currently it suppots the features listed below:
 
@@ -7,4 +7,43 @@ Currently it suppots the features listed below:
 Any asset can create a new entry in the game localization file by simple adding a folder called `kf_translationFiles` in the workshop export folder. The file format is the same from Commons Includable, and support the languages listed there.
 By default, all entries are prefixed with `KF_` string. To avoid it (to override a game default locale entry) the entry should start with a `%` symbol, like described in the Commons Includable's main page.
 
-### More features may be added in a future
+## Texture atlas engine (NEW IN 1.1)
+
+### Per asset file addition
+
+Any asset can create new items inside game default texture atlas, for use in UI as example. Add a folder called `kf_imageFiles` in the workshop export folder and put all images in **PNG format** inside.
+By default, all sprite entries are prefixed with `K45_PACK.{workshopId_17digits}_`, this is used to prevent collision between asset images. If you **REALLY** want to use the name as is, put a `%` symbol in the start of the name.
+
+
+#### Image borders configurations 
+If necessary you can add a file in the folder called `bordersDescriptor.txt` to describe the borders of each sprite. The file format is:
+
+```json
+{filenameWithoutExtension}={left},{right},{top},{bottom}
+```
+
+Use one line per image file. The border descriptor is used to tell the game which part of the image will not be rescaled when using a greater dimension. (Useful for panels)
+
+### Local sprites
+
+Additionally, you can add your own sprites in a local folder for personal use. It's located in the path `{CS_APPDATA_FOLDER}/Klyte45Mods/kf_imageFiles`. By default, all sprite names are prefixed with `K45_KF_LOCAL_`, but can be escaped starting with a `%` as well.
+
+### Export game sprites
+
+All game sprites can be exported clicking the button `Export game images` in mod options panel in pause/options menu.
+All files are exported in a compatible format to be reimported into the game (name escaped + borders descriptors) and all them are exported to folder `{CS_APPDATA_FOLDER}/Klyte45Mods/kf_exportedAtlas`.
+
+***WARNING: THIS FOLDER IS ERASED BEFORE EVERY EXPORT!*** So make sure to put your edited files into the import folder or in another safe place to prevent losing your work...
+
+
+## Text-in-inline-texture (NEW IN 1.1)
+
+Now is also possible to draw a sprite with some text inside it inside a text in the game UI, like the TLM line icons. Use the following tag in your text:
+ ```xml
+<k45LineSymbol {spriteName},{bgcolorRGBnoHashtag},{textToWrite}>
+```
+
+Note that there's no space between the commas. The UI item must be allowed to process markups to this work!
+Avoid using more than 7 characters in the text...
+
+### More features may be added in the future...
